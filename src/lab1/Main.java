d308ae1a4651d6d39d018e3e27fffe0a67154e0c
Kosi <kosiokaro@gmail.com>
2025-09-09 12:34:53 -0400

    main

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -1,4 +1,10 @@
 package lab1;
 
-public class Main {
+public class Main{
+    public static void main(String[] args) {
+        FizzBuzz fb = new FizzBuzz();
+        FizzBuzz.doFizzBuzz(5);
+        FizzBuzz.main(args);
+    }
 }
+
